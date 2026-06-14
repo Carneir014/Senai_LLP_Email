69a1e24fa1164934b3303035b024ff721c3c874f
Carneir014 <jfatesg@gmail.com>
2026-06-14 16:10:09 -0300

    Correção

--- Senai_LLP_Email/Emails.java
+++ Senai_LLP_Email/Emails.java
@@ -1,44 +1,39 @@
 package Senai_LLP_Email;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Emails {
 
-    public static int numEmailsUnicos(String[] emails) {
-        Set<String> unicos = new HashSet<>();
+    public static String normalizar(String email) {
+        String[] partes   = email.split("@");
+        String nomeLocal  = partes[0];
+        String dominio    = partes[1];
 
-        for (String email : emails) {
-            String[] partes = email.split("@");
-            String nomeLocal = partes[0];
-            String dominio   = partes[1];
-
-            // Ignora tudo depois do primeiro '+'
-            if (nomeLocal.contains("+")) {
-                nomeLocal = nomeLocal.substring(0, nomeLocal.indexOf("+"));
-            }
-
-            // Remove todos os pontos do nome local
-            nomeLocal = nomeLocal.replace(".", "");
-
-            unicos.add(nomeLocal + "@" + dominio);
+        if (nomeLocal.contains("+")) {
+            nomeLocal = nomeLocal.substring(0, nomeLocal.indexOf("+"));
         }
 
-        return unicos.size();
+        nomeLocal = nomeLocal.replace(".", "");
+
+        return nomeLocal + "@" + dominio;
     }
 
     public static void main(String[] args) {
-        String[] emails1 = {
-            "test.email+alex@leetcode.com",
-            "test.e.mail+bob.cathy@leetcode.com",
-            "testemail+david@lee.tcode.com"
-        };
-        System.out.println(numEmailsUnicos(emails1)); // 2
+        Scanner sc = new Scanner(System.in);
+        Set<String> unicos = new HashSet<>();
 
-        String[] emails2 = {
-            "a@leetcode.com",
-            "b@leetcode.com",
-            "c@leetcode.com"
-        };
-        System.out.println(numEmailsUnicos(emails2)); // 3
+        System.out.println("Digite os e-mails (deixe em branco para encerrar):");
+
+        while (true) {
+            System.out.print(">> E-mail: ");
+            String entrada = sc.nextLine().trim();
+
+            if (entrada.isEmpty()) break;
+
+            unicos.add(normalizar(entrada));
+        }
+
+        System.out.println("\nEndereços únicos que receberam e-mail: " + unicos.size());
     }
 }
